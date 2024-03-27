d1f745c7feee82231c294f4dd330397f91a005ca
yosiaevan <yosiaevan23@gmail.com>
2024-03-27 11:57:56 +0700

    membuat satu data fasilitas

--- app/src/main/java/com/example/sportbooker/DBHandler.java
+++ app/src/main/java/com/example/sportbooker/DBHandler.java
@@ -80,7 +80,7 @@ public class DBHandler extends SQLiteOpenHelper {
                 COL_FACILITIES_OPENING_HOUR + ", " +
                 COL_FACILITIES_CLOSING_HOUR + ", " +
                 COL_FACILITIES_PRICE + ") VALUES ('F01', 'Footbal Field A', 'Inside', '08:00', '21:00', '50000')";
-        String insertFacility1 = "INSERT INTO " + FACILITIES_TABLE + " (" +
+        String insertFacility2 = "INSERT INTO " + FACILITIES_TABLE + " (" +
                 COL_FACILITIES_ID + ", " +
                 COL_FACILITIES_NAME + ", " +
                 COL_FACILITIES_TYPE + ", " +
